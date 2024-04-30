83c12ac4f28d5537b6daa88deb0043ff583724bc
saadbaseer <161059131+saadbaseer@users.noreply.github.com>
2024-04-30 23:38:37 +0500

    initial commit

---
+++ day-8/Q.22/main.js
@@ -0,0 +1,6 @@
+// Question 22: 
+// Intentional Error: Try to produce an array 
+// index error in one of your programs.Correct the error before finishing.
+var cities = ['Karachi', 'Lahore', 'Pindi'];
+console.log(cities[3]); // this is an error bcz array starts form 0
+console.log(cities[2]); // i corrected it here 
