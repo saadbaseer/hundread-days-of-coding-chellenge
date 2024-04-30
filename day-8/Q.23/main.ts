dc3cd2baef1cb54f2b15fa821209cfe226b5d0b4
saadbaseer <161059131+saadbaseer@users.noreply.github.com>
2024-04-30 23:46:01 +0500

    updated

--- day-8/Q.23/main.ts
+++ day-8/Q.23/main.ts
@@ -2,30 +2,13 @@
 // Conditional Tests: Write a series of conditional tests.
 //  Predict the results of each test.
 
-// Equality with strings
-console.log("Testing equality with strings:");
-console.log("apple" == "apple"); // True
-console.log("apple" == "Apple"); // False
 
-// Using the lower case function
-console.log("Testing with lower case:");
-console.log("Apple".toLowerCase() == "apple"); // True
+let food ="biryani"
+console.log("if food is biryani i predict true rather then false");
+console.log(food=="biryani");
 
-// Numerical tests
-console.log("Numerical tests:");
-console.log(10 > 5); // True
-console.log(2 < 1); // False
+console.log('if food is not biryani then i predict false');
+console.log(food=="nihari");
 
-// Tests using "and" and "or" operators
-console.log("Tests with 'and' and 'or':");
-console.log(true && false); // False
-console.log(true || false); // True
 
-// Test whether an item is in a array
-let fruits = ["apple", "banana", "cherry"];
-console.log("Is 'apple' in fruits?");
-console.log(fruits.includes("apple")); // True
 
-// Test whether an item is not in a array
-console.log("Is 'mango' not in fruits?");
-console.log(!fruits.includes("mango")); // false
